f0c900e7fa42eff0eadfe7d8de9113c00c6029fe
Balazs Sevecsek <mountain@tari.com>
2024-12-26 19:39:58 +0100

    Remove unnecessary files

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import { promises as fs } from "fs";
 import * as theme from "jsonresume-theme-even";
 import puppeteer from "puppeteer";
 import { render } from "resumed";
-import express from "express";
 
 const PORT = 4000;
 
@@ -12,37 +11,16 @@ function sleep(ms) {
   });
 }
 
-//serve static files
-const fileName = "balazs_sevecsek.jpg";
-const app = express();
-app.use(express.static("public"));
-const server = app.listen(PORT, () => {
-  console.log(`static file server listening on port ${PORT}`);
-});
-
 const resumeJson = await fs.readFile("resume.json");
 const resume = JSON.parse(resumeJson, "utf-8");
 const html = await render(resume, theme);
-const updatedHtml = html
-  .replace(
-    /<\/head>/,
-    `
-  <link href="/path/to/print.css" media="print" rel="stylesheet" />
-  </head>`
-  )
-  .replace(/<img\s+[^>]*src="([^"]*)"[^>]*>/g, (match, p1) => {
-    return match.replace(
-      p1,
-      `http://localhost:${PORT}/${fileName}`.replace(/\\/g, "/")
-    );
-  });
 
 const browser = await puppeteer.launch({
   // headless: false,
 });
 const page = await browser.newPage();
 
-await page.setContent(updatedHtml, { waitUntil: "networkidle0" });
+await page.setContent(html, { waitUntil: "networkidle0" });
 await page.evaluate(() => {
   const images = document.querySelectorAll("img");
   images.forEach((img) => {
@@ -60,5 +38,4 @@ await page.pdf({
   format: "a4",
   printBackground: true,
 });
-server.close();
 await browser.close();
